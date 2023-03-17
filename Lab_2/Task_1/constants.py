14eaf6b30b238265d66241e191df549b8f351d42
Ulyana <ulya.sidorova@gmail.com>
2023-03-18 00:05:21 +0300

    modify n-gram function and constants file

--- Lab_2/Task_1/constants.py
+++ Lab_2/Task_1/constants.py
@@ -1,5 +1,6 @@
-rxForSentences = r'((([a-x0-9\)][\.])|(\.\.\.)|(([\?!]+)))[\'\"\)]?[\s+\n][A-X0-9\'\"\(])|(\.[\"\'\)]?$)'
+rxForSentences = r"((([a-x0-9\)][\.])|(\.\.\.)|(([\?!]+)))['\"\)]?" \
+                 r"[\s+\n][A-X0-9'\"\(])|(((\.)|((\?)|(!)))[\"'\)]?\s*$)"
 rxForNameAbbreviations = r'(\b([Mm]r|[Mm]rs|[Dd]r|[Ll]t|[Rr]ep)\.)'
-rxForNonDeclarative = r'[\?!]+'
+rxForNonDeclarative = r'(((([\?!]+)))[\'\"\)]?[\s+\n][A-X0-9\'\"\(])|([\?!][\"\'\)]?\s*$)'
 rxForWords = r'\b[\w]+\b'
 rxForNumbers = r'\b[0-9]+\b'
--- Lab_2/Task_1/functions.py
+++ Lab_2/Task_1/functions.py
@@ -1,37 +1,17 @@
 import re
 from constants import rxForSentences, rxForNameAbbreviations, rxForNonDeclarative, rxForWords, rxForNumbers
+from helpFunctions import textLength, listOfWords
 
 
 def countOfSentences(text: str):
     notASentence = re.findall(rxForNameAbbreviations, text)
     matches = re.findall(rxForSentences, text)
-    print(matches)
     return len(matches) - len(notASentence)
 
 
 def nonDeclarativeSentences(text: str):
     matches = re.findall(rxForNonDeclarative, text)
     return len(matches)
-
-
-def textLength(text: str):
-    wordMatches = re.findall(rxForWords, text)
-    numbersMatches = re.findall(rxForNumbers, text)
-    textLen = 0
-    for word in wordMatches:
-        if not (word in numbersMatches):
-            textLen = textLen + len(word)
-    return textLen
-
-
-def listOfWords(text: str):
-    wordMatches = re.findall(rxForWords, text)
-    numbersMatches = re.findall(rxForNumbers, text)
-    textLen = []
-    for word in wordMatches:
-        if not (word in numbersMatches):
-            textLen.append(word)
-    return textLen
 
 
 def averageLengthOfSentence(text: str):
@@ -43,12 +23,12 @@ def averageLengthOfWord(text: str):
     return textLength(text) / len(listOfWords(text)) if len(listOfWords(text)) != 0 else 0
 
 
-def topKRepeatedNGrams(text: str, k=10, n=4):
+def topKRepeatedWordNGrams(text: str, k=10, n=4):
     dictionary = {}
     listOfWord = listOfWords(text)
     countOfWord = len(listOfWords(text))
     if countOfWord < n:
-        print("")
+        print(f"n = {n} more than the number of words({countOfWord}) in the text")
     else:
         for i in range(0, countOfWord - n + 1):
             combination = ' '.join(listOfWord[i:i+n])
@@ -60,3 +40,27 @@ def topKRepeatedNGrams(text: str, k=10, n=4):
     if len(dictionary) <= k:
         return dictionary
     return dictionary[0: k]
+
+
+def topKRepeatedSymbolNGrams(text: str, k=10, n=4):
+    dictionary = {}
+    listOfWord = listOfWords(text)
+    countOfWord = len(listOfWords(text))
+    lenOfText = textLength(text)
+    if lenOfText < n:
+        print(f"n = {n} more than the number of symbols({lenOfText}) in the text")
+    else:
+        for i in range(0, countOfWord):
+            if len(listOfWord[i]) < n:
+                continue
+            for j in range(0, len(listOfWord[i]) - n + 1):
+                combination = listOfWord[i][j:j+4]
+                if combination not in dictionary:
+                    dictionary[combination] = 1
+                else:
+                    dictionary[combination] += 1
+    dictionary = sorted(dictionary.items(), key=lambda x: x[1], reverse=True)
+    if len(dictionary) <= k:
+        return dictionary
+    return dictionary[0: k]
+
