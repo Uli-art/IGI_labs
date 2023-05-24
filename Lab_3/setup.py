a658ac8324e294869eeabd70f43101910e0a8e24
Ulyana <ulya.sidorova@gmail.com>
2023-05-24 13:16:57 +0300

    Add setup

---
+++ Lab_3/setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='uly-serializer',
+    version='0.0.1',
+    packages=[
+        "ObjectSerializer"
+    ],
+    license='MIT',
+    author='Ulyana',
+    author_email='ulya.sidorova@gmail.com',
+    description='Python JSON and XML serializer',
+)
